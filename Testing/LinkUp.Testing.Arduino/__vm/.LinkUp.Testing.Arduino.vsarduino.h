803c90186c6734a8c4c9b529b9e77c79fdd3a81a
Thomas Weichselbaumer <tom.weichselbaumer@gmail.com>
2017-02-12 15:41:46 +0100

    basic changes

--- Testing/LinkUp.Testing.Arduino/__vm/.LinkUp.Testing.Arduino.vsarduino.h
+++ Testing/LinkUp.Testing.Arduino/__vm/.LinkUp.Testing.Arduino.vsarduino.h
@@ -5,95 +5,52 @@
 	        all non-arduino files created by visual micro and all visual studio project or solution files can be freely deleted and are not required to compile a sketch (do not delete your own code!).
 	        note: debugger breakpoints are stored in '.sln' or '.asln' files, knowledge of last uploaded breakpoints is stored in the upload.vmps.xml file. Both files are required to continue a previous debug session without needing to compile and upload again
 	
-	Hardware: Generic STM32F103C series, Platform=STM32F1, Package=Arduino_STM32
+	Hardware: Teensy 3.2 / 3.1, Platform=teensy3, Package=teensy
 */
 
 #ifndef _VSARDUINO_H_
 #define _VSARDUINO_H_
-#define DEBUG_LEVEL DEBUG_NONE
-#define BOARD_generic_stm32f103c
-#define VECT_TAB_ADDR 0x8002000
-#define ERROR_LED_PORT GPIOB
-#define ERROR_LED_PIN 1
-#define F_CPU 72000000L
+#define __HARDWARE_MK20dx256__
+#define __HARDWARE_MK20DX256__
+#define __MK20DX256__
+#define TEENSYDUINO 131
 #define ARDUINO 106011
-#define ARDUINO_GENERIC_STM32F103C
-#define ARDUINO_ARCH_STM32F1
-#define SERIAL_USB
-#define GENERIC_BOOTLOADER
-#define CONFIG_MAPLE_MINI_NO_DISABLE_DEBUG 1
-#define MCU_STM32F103C8
-#define __STM32F1__
+#define F_CPU 96000000
+#define USB_SERIAL
+#define LAYOUT_US_ENGLISH
 #define __cplusplus 201103L
-#define GCC_VERSION 40803
-#define __inline__
-#define __asm__(x)
+#define __arm__
+#define __ARM__
+#define __extension__
+#define  __attribute__(x)
+typedef void *__builtin_va_list;
 #define __extension__
 #define __ATTR_PURE__
 #define __ATTR_CONST__
 #define __inline__
+#define __asm__ 
 #define __volatile__
+#define _HAVE_STDC
 
-
-#define __ICCARM__
-#define __ASM
-#define __INLINE
-#define __builtin_va_list void
-//#define _GNU_SOURCE 
-//#define __GNUC__ 0
-//#undef  __ICCARM__
-//#define __GNU__
-
-typedef long Pio;
-typedef long Efc;
-typedef long Adc;
-typedef long Pwm;
-typedef long Rtc;
-typedef long Rtt;
-typedef long pRtc;
-typedef long Spi;
-typedef long spi;
-typedef long Ssc;
-//typedef long p_scc;
-typedef long Tc;
-//typedef long pTc;
-typedef long Twi;
-typedef long Wdt;
-//typedef long pTwi;
-typedef long Usart;
-typedef long Pdc;
-typedef long Rstc;
-
-extern const int ADC_MR_TRGEN_DIS = 0;
-extern const int ADC_MR_TRGSEL_ADC_TRIG0 = 0;
-extern const int ADC_MR_TRGSEL_Pos = 0;
-
-extern const int ADC_MR_TRGSEL_Msk = 0;
-extern const int ADC_MR_TRGEN = 0;
-extern const int ADC_TRIG_TIO_CH_0 = 0;
-extern const int ADC_MR_TRGSEL_ADC_TRIG1 = 0;
-extern const int ADC_TRIG_TIO_CH_1 = 0;
-extern const int ADC_MR_TRGSEL_ADC_TRIG2 = 0;
-extern const int ADC_MR_TRGSEL_ADC_TRIG3 = 0;
-
-#define __ARMCC_VERSION 400678
-#define __attribute__(noinline)
-
-#define prog_void
-#define PGM_VOID_P int
-
-
-            
-typedef unsigned char byte;
-extern "C" void __cxa_pure_virtual() {;}
-
-
+#define NEW_H
 
 #include <arduino.h>
-#include <pins_arduino.h> 
-#include <variant.h> 
-#include <board.cpp> 
-#undef cli
-#define cli()
+#define __arm__
+#define __ARM__
+#define __extension__
+#define  __attribute__(x)
+typedef void *__builtin_va_list;
+#define __extension__
+#define __ATTR_PURE__
+#define __ATTR_CONST__
+#define __inline__
+#define __asm__ 
+#define __volatile__
+
+#define __disable_irq() __asm__ volatile("");
+#define __enable_irq()	__asm__ volatile("");
+
+
+#define NEW_H
 #include "Testing.ino"
 #endif
