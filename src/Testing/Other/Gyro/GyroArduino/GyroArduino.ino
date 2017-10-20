4441476d3c8ee43e4ff7c2f2205d53fcdea5f73d
Thomas Weichselbaumer <tom.weichselbaumer@gmail.com>
2017-10-20 16:59:08 +0200

    add example project gyro

---
+++ src/Testing/Other/Gyro/GyroArduino/GyroArduino.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		GyroArduino.ino
+ Created:	10/20/2017 4:55:32 PM
+ Author:	thoma
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
