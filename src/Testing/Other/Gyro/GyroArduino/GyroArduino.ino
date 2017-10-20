e8e63622a29e689e9704b2886d4b56ca31dc376c
Thomas Weichselbaumer <tom.weichselbaumer@gmail.com>
2017-10-20 20:12:02 +0200

    update gyro basic sample

--- src/Testing/Other/Gyro/GyroArduino/GyroArduino.ino
+++ src/Testing/Other/Gyro/GyroArduino/GyroArduino.ino
@@ -1,15 +1,84 @@
-/*
- Name:		GyroArduino.ino
- Created:	10/20/2017 4:55:32 PM
- Author:	thoma
-*/
+#include "LinkUpRaw.h"
+#include "MPU9250.h"
 
-// the setup function runs once when you press reset or power the board
-void setup() {
+#define BUFFER_SIZE 1024
 
+#define DebugStream Serial
+#define DataStream Serial1
+
+#define DebugBaud 115200
+#define DataBaud 250000
+
+#define PinLed 13
+
+uint8_t nLedStatus = 1;
+
+uint8_t pBuffer[BUFFER_SIZE];
+uint32_t nLastTicks = 0;
+uint32_t nLastTicksFast = 0;
+LinkUpRaw linkUpConnector;
+
+MPU9250 IMU((uint8_t)0x68, (uint8_t)0);
+
+
+int beginStatus;
+
+struct DATA {
+	uint32_t time;
+	int16_t gx;
+	int16_t gy;
+	int16_t gz;
+	int16_t ax;
+	int16_t ay;
+	int16_t az;
+	int16_t mx;
+	int16_t my;
+	int16_t mz;
+	int16_t t;
+	int16_t p;
+};
+
+DATA data;
+
+void setup()
+{
+	DebugStream.begin(DebugBaud);
+	DebugStream.setTimeout(1);
+	DataStream.begin(DataBaud);
+	DataStream.setTimeout(1);
+
+	pinMode(PinLed, OUTPUT);
+
+	IMU.begin(ACCEL_RANGE_4G, GYRO_RANGE_250DPS);
 }
 
-// the loop function runs over and over again until power down or reset
-void loop() {
-  
+void loop()
+{
+	uint32_t nBytesToSend;
+	uint32_t nTime = micros();
+
+	if (nTime - nLastTicks > 1000 * 1000 * 1)
+	{
+		nLastTicks = nTime;
+		nLedStatus = !nLedStatus;
+		digitalWrite(PinLed, nLedStatus);
+	}
+
+	if (nTime - nLastTicksFast > 1000 * 5 && IMU.checkDataReady())
+	{
+		nLastTicksFast = nTime;
+		data.time = nTime;
+		IMU.getMotion10Counts(&data.ax, &data.ay, &data.az, &data.gx, &data.gy, &data.gz, &data.mx, &data.my, &data.mz, &data.t);
+		LinkUpPacket packet;
+		packet.pData = (uint8_t*)malloc(sizeof(DATA));
+		*(DATA*)packet.pData = data;
+		packet.nLength = sizeof(DATA);
+		linkUpConnector.send(packet);
+	}
+
+	nBytesToSend = linkUpConnector.getRaw(pBuffer, BUFFER_SIZE);
+
+	if (nBytesToSend > 0) {
+		DataStream.write(pBuffer, nBytesToSend);
+	}
 }
--- src/Testing/Other/Gyro/GyroWpf/MainWindow.xaml.cs
+++ src/Testing/Other/Gyro/GyroWpf/MainWindow.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using LinkUp.Raw;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO.Ports;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +23,40 @@ namespace GyroWpf
     /// </summary>
     public partial class MainWindow : Window
     {
+        const string DATA_PORT = "COM4";
+        const string DEBUG_PORT = "COM3";
+
+        const int DATA_BAUD = 250000;
+        const int DEBUG_BAUD = 250000;
+
+        SerialPort port;
+        LinkUpSerialPortConnector connector;
+        Stopwatch watch;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            watch = new Stopwatch();
+            watch.Start();
+
+            port = new SerialPort(DEBUG_PORT, DEBUG_BAUD);
+            port.Open();
+            port.ReadExisting();
+            port.DataReceived += Port_DataReceived;
+
+            connector = new LinkUpSerialPortConnector(DATA_PORT, DATA_BAUD);
+            connector.ReveivedPacket += Connector_ReveivedPacket;
+        }
+
+        private void Connector_ReveivedPacket(LinkUpConnector connector, LinkUpPacket packet)
+        {
+            Console.WriteLine("{0} - Receive:\n\t{1}", watch.ElapsedTicks * 1000 / Stopwatch.Frequency, string.Join(" ", packet.Data.Select(b => string.Format("{0:X2} ", b))));
+        }
+
+        private void Port_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+
         }
     }
 }
