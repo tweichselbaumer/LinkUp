36ff6dde9f672b2857c64899a1ac97bd08fd43ec
Thomas Weichselbaumer <thomas.weichselbaumer@hotmail.com>
2018-01-23 15:02:24 +0100

    refactor project add android client

--- src/Testing/Example/Example3/Example3Arduino/__vm/.Example3Arduino.vsarduino.h
+++ src/Testing/Example/Example3/Example3Arduino/__vm/.Example3Arduino.vsarduino.h
@@ -1,21 +1,22 @@
 /* 
 	Editor: http://www.visualmicro.com
-	        visual micro and the arduino ide ignore this code during compilation. this code is automatically maintained by visualmicro, manual changes to this file will be overwritten
-	        the contents of the Visual Micro sketch sub folder can be deleted prior to publishing a project
-	        all non-arduino files created by visual micro and all visual studio project or solution files can be freely deleted and are not required to compile a sketch (do not delete your own code!).
-	        note: debugger breakpoints are stored in '.sln' or '.asln' files, knowledge of last uploaded breakpoints is stored in the upload.vmps.xml file. Both files are required to continue a previous debug session without needing to compile and upload again
+			visual micro and the arduino ide ignore this code during compilation. this code is automatically maintained by visualmicro, manual changes to this file will be overwritten
+			the contents of the Visual Micro sketch sub folder can be deleted prior to publishing a project
+			all non-arduino files created by visual micro and all visual studio project or solution files can be freely deleted and are not required to compile a sketch (do not delete your own code!).
+			note: debugger breakpoints are stored in '.sln' or '.asln' files, knowledge of last uploaded breakpoints is stored in the upload.vmps.xml file. Both files are required to continue a previous debug session without needing to compile and upload again
 	
 	Hardware: Teensy 3.2 / 3.1, Platform=teensy3, Package=teensy
 */
+
+#if defined(_VMICRO_INTELLISENSE)
 
 #ifndef _VSARDUINO_H_
 #define _VSARDUINO_H_
 #define __HARDWARE_MK20dx256__
 #define __HARDWARE_MK20DX256__
-#define _VMDEBUG 1
 #define __MK20DX256__
 #define TEENSYDUINO 140
-#define ARDUINO 10801
+#define ARDUINO 10805
 #define F_CPU 96000000
 #define USB_SERIAL
 #define LAYOUT_US_ENGLISH
@@ -35,7 +36,7 @@ typedef void *__builtin_va_list;
 
 #define NEW_H
 
-#include <arduino.h>
+#include <wprogram.h>
 #define __arm__
 #define __ARM__
 #define __extension__
@@ -54,4 +55,5 @@ typedef void *__builtin_va_list;
 
 #define NEW_H
 #include "Example3Arduino.ino"
+#endif
 #endif
