162f0c1b1a652b7b3f3b1a71ad27bd00d6d58558
Thomas Weichselbaumer <thomas.weichselbaumer@hotmail.com>
2023-02-24 09:47:44 +0100

    clean up project

--- src/Testing/Example/Example2/LinkUp.Example2.Net45/Program.cs
+++ src/Testing/Example/Example2/LinkUp.Example2.Net45/Program.cs
@@ -1,4 +1,29 @@
-﻿using LinkUp.Raw;
+﻿/********************************************************************************
+ * MIT License
+ *
+ * Copyright (c) 2023 Thomas Weichselbaumer
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ ********************************************************************************/
+
+using LinkUp.Raw;
 using System;
 using System.Diagnostics;
 using System.IO.Ports;
@@ -7,65 +32,65 @@ using System.Threading;
 
 namespace LinkUp.Example2.Net45
 {
-    internal class Program
-    {
-        private const int DATA_BAUD = 250000;
-        private const string DATA_PORT = "COM6";
-        private const int DEBUG_BAUD = 250000;
-        private const string DEBUG_PORT = "COM3";
-        private static Stopwatch watch;
+	internal class Program
+	{
+		private const int DATA_BAUD = 250000;
+		private const string DATA_PORT = "COM6";
+		private const int DEBUG_BAUD = 250000;
+		private const string DEBUG_PORT = "COM3";
+		private static Stopwatch watch;
 
-        private static void Connector_ReveivedPacket(LinkUpConnector connector, LinkUpPacket packet)
-        {
-            lock (Console.Out)
-            {
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.WriteLine("{0} - Receive:\n\t{1}", watch.ElapsedTicks * 1000 / Stopwatch.Frequency, string.Join(" ", packet.Data.Select(b => string.Format("{0:X2} ", b))));
-                Console.ResetColor();
-            }
-        }
+		private static void Connector_ReveivedPacket(LinkUpConnector connector, LinkUpPacket packet)
+		{
+			lock (Console.Out)
+			{
+				Console.ForegroundColor = ConsoleColor.Yellow;
+				Console.WriteLine("{0} - Receive:\n\t{1}", watch.ElapsedTicks * 1000 / Stopwatch.Frequency, string.Join(" ", packet.Data.Select(b => string.Format("{0:X2} ", b))));
+				Console.ResetColor();
+			}
+		}
 
-        private static void Main(string[] args)
-        {
-            byte[] data = new byte[] { 0x99, 0x0A, 0x01, 0xAA, 0x01, 0xAA, 0xAA, 0x01, 0xAA, 0x05, 0x55,
-                                       0x8A, 0x55, 0xB9, 0x55, 0x75, 0x01, 0x02, 0x6F, 0xFF, 0x99, 0x99,
-                                       0x0A, 0x01, 0xAA, 0x01, 0xAA, 0xAA, 0x01, 0xAA, 0x05, 0x55, 0x8A,
-                                       0x55, 0xB9, 0x55, 0x75, 0x01, 0x02, 0x6F, 0xFF, 0x99 };
+		private static void Main(string[] args)
+		{
+			byte[] data = new byte[] { 0x99, 0x0A, 0x01, 0xAA, 0x01, 0xAA, 0xAA, 0x01, 0xAA, 0x05, 0x55,
+									   0x8A, 0x55, 0xB9, 0x55, 0x75, 0x01, 0x02, 0x6F, 0xFF, 0x99, 0x99,
+									   0x0A, 0x01, 0xAA, 0x01, 0xAA, 0xAA, 0x01, 0xAA, 0x05, 0x55, 0x8A,
+									   0x55, 0xB9, 0x55, 0x75, 0x01, 0x02, 0x6F, 0xFF, 0x99 };
 
-            watch = new Stopwatch();
-            watch.Start();
+			watch = new Stopwatch();
+			watch.Start();
 
-            SerialPort port = new SerialPort(DEBUG_PORT, DEBUG_BAUD);
-            port.Open();
-            port.ReadExisting();
-            port.DataReceived += Port_DataReceived;
+			SerialPort port = new SerialPort(DEBUG_PORT, DEBUG_BAUD);
+			port.Open();
+			port.ReadExisting();
+			port.DataReceived += Port_DataReceived;
 
-            LinkUpSerialPortConnector connector = new LinkUpSerialPortConnector(DATA_PORT, DATA_BAUD);
-            connector.ReveivedPacket += Connector_ReveivedPacket;
+			LinkUpSerialPortConnector connector = new LinkUpSerialPortConnector(DATA_PORT, DATA_BAUD);
+			connector.ReveivedPacket += Connector_ReveivedPacket;
 
-            for (int i = 0; i < 5; i++)
-            {
-                watch.Restart();
-                connector.SendPacket(new LinkUpPacket() { Data = data });
-                Console.WriteLine("{0} - Send", watch.ElapsedTicks * 1000 / Stopwatch.Frequency);
-                Thread.Sleep(1000);
-            }
+			while (true)
+			{
+				watch.Restart();
+				connector.SendPacket(new LinkUpPacket() { Data = data });
+				Console.WriteLine("{0} - Send", watch.ElapsedTicks * 1000 / Stopwatch.Frequency);
+				Thread.Sleep(1000);
+			}
 
-            Console.Read();
-        }
+			Console.Read();
+		}
 
-        private static void Port_DataReceived(object sender, SerialDataReceivedEventArgs e)
-        {
-            if (sender is SerialPort)
-            {
-                SerialPort port = (SerialPort)sender;
-                lock (Console.Out)
-                {
-                    Console.ForegroundColor = ConsoleColor.Cyan;
-                    Console.WriteLine("{0} - Debug:\t{1}", watch.ElapsedTicks * 1000 / Stopwatch.Frequency, port.ReadExisting());
-                    Console.ResetColor();
-                }
-            }
-        }
-    }
+		private static void Port_DataReceived(object sender, SerialDataReceivedEventArgs e)
+		{
+			if (sender is SerialPort)
+			{
+				SerialPort port = (SerialPort)sender;
+				lock (Console.Out)
+				{
+					Console.ForegroundColor = ConsoleColor.Cyan;
+					Console.WriteLine("{0} - Debug:\t{1}", watch.ElapsedTicks * 1000 / Stopwatch.Frequency, port.ReadExisting());
+					Console.ResetColor();
+				}
+			}
+		}
+	}
 }
--- src/Testing/Example/Example7/LinkUp.Example7.Cpp/main.cpp
+++ src/Testing/Example/Example7/LinkUp.Example7.Cpp/main.cpp
@@ -1,3 +1,27 @@
+/********************************************************************************
+ * MIT License
+ *
+ * Copyright (c) 2023 Thomas Weichselbaumer
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ ********************************************************************************/
 #include <windows.h>
 #include <stdio.h>
 #include <conio.h>
@@ -8,69 +32,69 @@
 #define BUFFER_SIZE 1024
 #define VALUES 1
 
-int __cdecl main(int argc, char **argv)
+int __cdecl main(int argc, char** argv)
 {
-	HANDLE hPipe = INVALID_HANDLE_VALUE;
-	uint8_t pBuffer[BUFFER_SIZE];
-	DWORD nBytesRead;
-	DWORD nTotalBytesAvail;
-	DWORD nBytesLeftThisMessag;
-	LinkUpRaw linkUpConnector;
-	DWORD nBytesToSend;
-	LinkUpNode linkUpNode = {};
-	LinkUpLabel* values[VALUES];
+   /*HANDLE hPipe = INVALID_HANDLE_VALUE;
+   uint8_t pBuffer[BUFFER_SIZE];
+   DWORD nBytesRead;
+   DWORD nTotalBytesAvail;
+   DWORD nBytesLeftThisMessag;
+   LinkUpRaw linkUpConnector;
+   DWORD nBytesToSend;
+   LinkUpNode linkUpNode = {};
+   LinkUpLabel* values[VALUES];
 
-	while (true)
-	{
-		if (hPipe == INVALID_HANDLE_VALUE)
-		{
-			hPipe = CreateFile(TEXT("\\\\.\\pipe\\linkup"), GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, 0, NULL);
-			if (hPipe != INVALID_HANDLE_VALUE) {
-				linkUpNode.init((char *)"node1");
-				for (int i = 0; i < VALUES; i++) {
-					char pName[200];
-					sprintf(pName, "value%d", i);
-					values[i] = linkUpNode.addLabel(pName, LinkUpLabelType::Int32);
-				}
-			}
-			Sleep(1);
-		}
+   while (true)
+   {
+      if (hPipe == INVALID_HANDLE_VALUE)
+      {
+         hPipe = CreateFile(TEXT("\\\\.\\pipe\\linkup"), GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, 0, NULL);
+         if (hPipe != INVALID_HANDLE_VALUE) {
+            linkUpNode.init((char *)"node1");
+            for (int i = 0; i < VALUES; i++) {
+               char pName[200];
+               sprintf(pName, "value%d", i);
+               values[i] = linkUpNode.addLabel(pName, LinkUpLabelType::Int32);
+            }
+         }
+         Sleep(1);
+      }
 
-		PeekNamedPipe(hPipe, pBuffer, 1024, &nBytesRead, &nTotalBytesAvail, &nBytesLeftThisMessag);
-		if (nTotalBytesAvail > 0)
-		{
-			if (ReadFile(hPipe, pBuffer, 1024, &nBytesRead, NULL))
-			{
-				if (nBytesRead > 0)
-				{
-					printf("%d - Receive:\n\t", micros() / 1000);
-					for (DWORD i = 0; i < nBytesRead; i++)
-					{
-						printf("%02X ", pBuffer[i]);
-					}
-					printf("\n");
-					linkUpNode.progress(pBuffer, nBytesRead);
-				}
-			}
-		}
-		else
-		{
-			linkUpNode.progress(NULL, 0);
-		}
+      PeekNamedPipe(hPipe, pBuffer, 1024, &nBytesRead, &nTotalBytesAvail, &nBytesLeftThisMessag);
+      if (nTotalBytesAvail > 0)
+      {
+         if (ReadFile(hPipe, pBuffer, 1024, &nBytesRead, NULL))
+         {
+            if (nBytesRead > 0)
+            {
+               printf("%d - Receive:\n\t", micros() / 1000);
+               for (DWORD i = 0; i < nBytesRead; i++)
+               {
+                  printf("%02X ", pBuffer[i]);
+               }
+               printf("\n");
+               linkUpNode.progress(pBuffer, nBytesRead);
+            }
+         }
+      }
+      else
+      {
+         linkUpNode.progress(NULL, 0);
+      }
 
-		nBytesToSend = linkUpNode.getRaw(pBuffer, BUFFER_SIZE);
+      nBytesToSend = linkUpNode.getRaw(pBuffer, BUFFER_SIZE);
 
-		if (nBytesToSend > 0)
-		{
-			printf("%d - Sent:\n\t", micros() / 1000);
-			for (DWORD i = 0; i < nBytesToSend; i++)
-			{
-				printf("%X ", pBuffer[i]);
-			}
-			printf("\n");
-			WriteFile(hPipe, pBuffer, nBytesToSend, 0, 0);
-		}
-	}
+      if (nBytesToSend > 0)
+      {
+         printf("%d - Sent:\n\t", micros() / 1000);
+         for (DWORD i = 0; i < nBytesToSend; i++)
+         {
+            printf("%X ", pBuffer[i]);
+         }
+         printf("\n");
+         WriteFile(hPipe, pBuffer, nBytesToSend, 0, 0);
+      }
+   }
 
-	return 0;
+   return 0;*/
 }
